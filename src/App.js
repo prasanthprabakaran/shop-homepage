c054f8a18c8f9b82474d21af08a8150ef3c5108d
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-06-26 22:40:59 +0530

    building

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 
 function App() {
